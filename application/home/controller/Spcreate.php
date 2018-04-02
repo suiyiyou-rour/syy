dc6a9eac1fcf5df4024841dc83517b8c53b87330
huangshiqi <409222855@qq.com>
2018-04-02 20:28:56 +0800

    供应商新建，列表，修改

--- application/home/controller/Spcreate.php
+++ application/home/controller/Spcreate.php
@@ -27,11 +27,50 @@ class Spcreate extends HomeBase
      * 供应商注册
      */
     public function spCreate(){
+        $data = input('post.');
+        $validate = \validate('Spcreate');
+        $res = $validate->check($data);
+        if (true !== $res) {
+            return \json(array("code" => 404, "msg" => $validate->getError()));
+        }
+        // 权限字段查看
+        if(empty($data['auth'])){
+            return \json(array("code" => 404, "msg" => '权限不能为空'));
+        }
+       
+        $user_code = $this->getCode();
         
+        $insertData=array(
+            'code'          => $user_code,
+            'type'          => 2,
+            'account_num'   => $data['account'],
+            'pwd'           => md5($data['password']),
+            'com_name'      => $data['company'],
+            'province'      => '福建',
+            'city'          => '福州',
+            'address'       => $data['address'],
+            'name'          => $data['name'],
+            'mobile'        => $data['mobile'],
+            'qq'            => 'xxx',
+            'email'         => $data['email_address'],
+            'open'          => 1,
+            'reg_time'      => time(),
+            'file'          => json_encode(array($data['y_image_path'],$data['z_image_path'],$data['s_image_path']))
+        );
 
-
-
-
+        $res1 = db('sp')->insert($insertData);
+        
+        $authData = [];
+        foreach($data['auth'] as $k => $v){
+            $authData[]=['user_code'=> $user_code,'fid' => $v];
+        }
+        $res2 = db('auth')->insertAll($authData);
+        // 插入返回
+        if($res1 && $res2){
+            return \json(array("code" => 200, "msg" => '添加成功'));
+        }else{
+            return \json(array("code" => 404, "msg" => '添加失败'));
+        }
     }
 
     /**
@@ -47,8 +86,10 @@ class Spcreate extends HomeBase
      * 供应商 code 生成
      */
      private function getCode(){
+       
         $code = cache('sp_code');
         $num = 1000000;
+        
         if($code){
             $num = $code;
         }
@@ -56,4 +97,120 @@ class Spcreate extends HomeBase
         cache('sp_code',$num);
         return $num;
      }
-}
+     /**
+      * 供应商列表
+      */
+      public function getSpList(){
+        $auth = input('id');
+        if(empty($auth)){
+            return \json(array('code' => 404 , 'msg' => '无权限访问'));
+        }
+
+        $page = input('post.page');
+        if(empty($page)){
+            $page = 1;
+        }
+        $page = ($page-1)*10;
+        $count = db('sp') ->where(array('type' => 2))->count();
+        $data = db('sp')->field(['pwd','reg_time','login_time'],true)->where(array('type' => 2))->limit($page.',10')->select();
+        $dataArr = [];
+        // 数据重构
+        foreach($data as $k =>$v){
+            $imgPath = json_decode($v['file']);
+            $temp = array(
+                'account'           =>   $v['account_num'],   //账号  
+                'password'          =>   '',   //密码   
+                'name'              =>   $v['name'],//供应商名字   
+                'mobile'            =>   $v['mobile'],//供应商联系方式     
+                'company'           =>   $v['com_name'],//公司名      
+                'address'           =>   $v['address'],//地址     
+                'y_image_path'      =>   $imgPath[0],//营业执照     
+                'z_image_path'      =>   $imgPath[1],//组织机构代码     
+                's_image_path'      =>   $imgPath[2],//税务登记证     
+                'email_address'     =>   $v['email'],//邮箱  
+                'open'              =>   $v['open'] 
+            );
+
+            $authData = db('auth')->field('fid')->where(array('user_code' => $v['code']))->select(); 
+            $auth = [];
+            foreach($authData as $key =>$val){
+                $auth[] = $val['fid'];
+            }
+            $temp['auth'] = $auth;
+            $dataArr[] = $temp;
+        }
+      
+        return \json(array('code' => 200 , 'data' => array('count'=> $count,'list' => $dataArr)));
+      }
+
+
+      /**
+       * 供应商启用
+       */
+      public function spOpen(){
+            $spCode = input('post.code');
+            $type = input('post.type');
+            // open 1 开启 2 关闭
+            $updateArr = ['open' => 1];
+            $str = '开启';
+            if($type == 'close'){
+                $updateArr['open'] = 2;
+                $str = '禁用';
+            }
+
+            $res = db('sp')->where(['code' => $spCode])->update($updateArr);
+            if($res){
+                return \json(array('code' => 200 , 'data' => $str.'成功！'));
+            }
+            return \json(array('code' => 404 , 'data' => $str.'失败！'));
+      }
+      /**
+       * 编辑修改
+       */
+      public function spChange(){
+            $data = input('post.');
+            $validate = \validate('Spcreate');
+            $res = $validate->check($data);
+            if (true !== $res) {
+                return \json(array("code" => 404, "msg" => $validate->getError()));
+            }
+            // 权限字段查看
+            if(empty($data['auth'])){
+                return \json(array("code" => 404, "msg" => '权限不能为空'));
+            }
+            // Code查看
+            if(empty($data['code'])){
+                return \json(array("code" => 404, "msg" => '修改失败，参数code缺失！'));
+            }
+            $user_code = $data['code'];
+            $insertData=array(
+                'code'          => $user_code,
+                'type'          => 2,
+                'account_num'   => $data['account'],
+                'pwd'           => md5($data['password']),
+                'com_name'      => $data['company'],
+                'address'       => $data['address'],
+                'name'          => $data['name'],
+                'mobile'        => $data['mobile'],
+                'qq'            => 'xxx',
+                'email'         => $data['email_address'],
+                'file'          => json_encode(array($data['y_image_path'],$data['z_image_path'],$data['s_image_path']))
+            );
+            // 数据更新
+            $res1 = db('sp')->where(['code' => $user_code])->update($insertData);
+            // 删除原权限
+            db('auth')->where(['user_code' => $user_code])->delete();
+
+            $authData = [];
+            foreach($data['auth'] as $k => $v){
+                $authData[]=['user_code'=> $user_code,'fid' => $v];
+            }
+            $res2 = db('auth')->insertAll($authData);
+            // 插入返回
+            if($res1 && $res2){
+                return \json(array("code" => 200, "msg" => '添加成功'));
+            }else{
+                return \json(array("code" => 404, "msg" => '添加失败'));
+            }
+      } 
+}   
--- application/home/validate/Spcreate.php
+++ application/home/validate/Spcreate.php
@@ -6,40 +6,51 @@ class Spcreate extends Validate
     //定义规则
     protected $rule = [
         //基本信息添加
-        'account'           =>   'require|between:6,15',   //账号  （主）必须
-        'password'          =>   'require|between:6,15',   //密码    （主）
-        'name'              =>   'require',//内部显示标题   （主）必须
-        'mobile'            =>   'require',//商品副标题     （主）
-        'company'           =>   'require',//服务保障      （副）
-        'address'           =>   'require',//路线类型     （副）
-        'y_image_path'      =>   'require',//游玩类型     （副）
-        'z_image_path'      =>  'require',//出发地     （副）必须
-        's_image_path'      =>  'require',//目的地     （副）必须
-        'email_address'     =>  'email'//主要景点     （副）必须
+        'account'           =>   'require|length:6,15',   //账号  
+        'password'          =>   'require|length:6,15',   //密码   
+        'name'              =>   'require',//供应商名字   
+        'mobile'            =>   'require',//供应商联系方式     
+        'company'           =>   'require',//公司名      
+        'address'           =>   'require',//地址     
+        'auth'              =>   'array',// 权限数组
+        'y_image_path'      =>   'require',//营业执照     
+        'z_image_path'      =>  'require',//组织机构代码     
+        's_image_path'      =>  'require',//税务登记证     
+        'email_address'     =>  'email'//邮箱     
     ];
 
     //反馈信息
     protected $message = [
         //基本信息添加
         'account.require'       => '账号是必须的',
-        'account.between'       => '账号长度限制为6-15个字符',
+        'account.length'        => '账号长度限制为6-15个字符',
         'password.require'      => '密码是必须的',
-        'password.between'      => '密码长度限制为6-15个字符',
-
-        'name.require'      => '名字是必须的',
-        'mobile.require'      => '手机是必须的',
-        'address.require'      => '密码是必须的',
-        'y_image_path.require'      => '密码是必须的',
-        'z_image_path.require'      => '密码是必须的',
-        's_image_path.require'      => '密码是必须的',
-        'email_address.email'      => '密码是必须的',
-
+        'password.length'       => '密码长度限制为6-15个字符',
+        'name.require'          => '名字是必须的',
+        'mobile.require'        => '手机是必须的',
+        'address.require'       => '地址是必须的',
+        'auth.array'            => '请选择权限',
+        'y_image_path.require'  => '营业执照是必须的',
+        'z_image_path.require'  => '组织机构代码证是必须的',
+        's_image_path.require'  => '税务登记证是必须的',
+        'email_address.email'   => '邮箱格式错误'
     ];
 
     //定义场景
     protected $scene = [
-        'addBasicInfo'   =>  ['contact_code', 'inside_code', 'inside_title', 'subtitle', 'service_type', 'line_type', 'play_type', 'begin_address', 'end_address', 'main_place', 'advance_time', 'online_type', 'on_time', 'off_time' , 'service_tel', 'refund_type', 'refund_info', 'rate'],
-    ];
+        'add' =>array(
+                'account'  ,   
+                'password',   
+                'name'  ,
+                'mobile' ,
+                'company'  ,
+                'address'  ,
+                'auth',
+                'y_image_path' ,
+                'z_image_path',
+                's_image_path' ,
+                'email_address')
+        ];
 
 
 }
