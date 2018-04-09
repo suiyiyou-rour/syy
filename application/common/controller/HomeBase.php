07964ed6c9cfb19a015bfe3b85401f1f9bc0b388
lzl1993 <398212600@qq.com>
2018-04-09 15:04:02 +0800

    更换环境 变量

--- application/common/controller/HomeBase.php
+++ application/common/controller/HomeBase.php
@@ -10,16 +10,15 @@ class HomeBase extends Base
         parent::__construct();
         $origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';
         $allow_origin = array(
-            'http://ajax.kris1945.com',
-            'http://localhost:8080',
-            'http://qq.kris1945.com'
+            'http://qq.suiyiyou.net',
+            'http://localhost:8080'
         );
         if(in_array($origin, $allow_origin)){
             header('Access-Control-Allow-Origin:'.$origin);                          // 指定允许其他域名访问
             header("Access-Control-Allow-Methods", "GET,POST");                     // 响应类型
             header('Access-Control-Allow-Headers:x-requested-with,content-type'); // 响应头设置
             header('Access-Control-Allow-Credentials:true');                        // 是否允许请求带有验证信息
-            cookie(['domain' => '.kris1945.com']);                                    // 初始化有效域名
+//            cookie(['domain' => '.suiyiyou.net']);                                    // 初始化有效域名
         }
         // 权限控制
         $controller = strtolower( request()->controller() );
--- application/config.php
+++ application/config.php
@@ -221,7 +221,7 @@ return [
         // cookie 保存路径
         'path'      => '/',
         // cookie 有效域名
-        'domain'    => '',
+        'domain'    => '.suiyiyou.net',
         //  cookie 启用安全传输
         'secure'    => false,
         // httponly设置
